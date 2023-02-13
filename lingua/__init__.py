6342db035c63505f33e2af67d2b93295610d40d9
Mark Coatsworth <mark.coatsworth@vectorinstitute.ai>
2023-02-13 16:57:54 -0500

    Updated __version__

--- lingua/__init__.py
+++ lingua/__init__.py
@@ -2,6 +2,6 @@ from .lingua_sdk import Client
 from .lingua_sdk import Model
 
 # SDK metadata
-__version__ = "0.0.1"
+__version__ = "0.2.0"
 __author__ = "Vector AI Engineering"
 __credits__ = "Vector Institute"

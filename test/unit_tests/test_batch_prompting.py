91d1775a129e30c7f96c63e480a88c7c9812baa7
Omkar Dige <omkar.dige@vectorinstitute.ai>
2023-03-06 17:04:38 -0500

    Changed model to OPT-175B

--- test/unit_tests/test_batch_prompting.py
+++ test/unit_tests/test_batch_prompting.py
@@ -6,7 +6,7 @@ class TestBatchPrompting:
 
     _host = "llm.cluster.local"
     _port = 4001
-    model_name = "OPT-6.7B"
+    model_name = "OPT-175B" # "OPT-6.7B"
     _client = lingua.Client(gateway_host=_host, gateway_port=_port)
     _model = _client.load_model(model_name)
     _requested_activation = ['decoder.layers.30.fc1']

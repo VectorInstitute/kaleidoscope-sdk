cd81f63f2bf2e8e9d2661167c6d2fd32609e41f8
jsiva <jsiva@llm.cluster.local>
2023-03-06 11:18:18 -0500

    Fix post unit test

--- test/unit/test_sdk.py
+++ test/unit/test_sdk.py
@@ -1,10 +1,11 @@
 import lingua
+import sys
 
 # Verifies the posted data is echoed correctly
 def test_post():
     """Verify post function"""
     response = lingua.utils.post("https://httpbin.org/post", "test post data")
-    assert response['data'] == "test post data"
+    assert response['data'] == 'test post data'
 
 def test_get():
     """Verify get function"""

41f66ef018f895554c4c4dd001a8e64aa2419590
jsiva <jsiva@llm.cluster.local>
2023-03-07 11:16:55 -0500

    Add response check test

--- test/unit/test_sdk.py
+++ test/unit/test_sdk.py
@@ -1,13 +1,15 @@
 import lingua
-import sys
+import pytest
 
 # Verifies the posted data is echoed correctly
 def test_post():
-    """Verify post function"""
     response = lingua.utils.post("https://httpbin.org/post", "test post data")
-    assert response['data'] == 'test post data'
+    assert response['data'] == "test post data"
 
 def test_get():
-    """Verify get function"""
     response = lingua.utils.get("http://ip.jsontest.com")
     assert response
+
+def test_check_response():
+    with pytest.raises(ValueError):
+        lingua.utils.get("https://httpbin.org/status/404")
